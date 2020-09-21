70de81768b1bb03beec7b68e33c9abcd9ed5c499
JoykishanSharma <joykishan120@gmail.com>
2020-09-21 09:00:12 +0530

    game logic created

---
+++ js/script.js
@@ -0,0 +1,24 @@
+//click on start/reset button
+    //if we are playing
+        //yes
+            //reload page
+        //no
+            //show trial left box
+            //change button text to "Rest Game"
+            //1. create a random fruit
+            //define a random step
+            //2. move fruit down one step every 30sec
+                //is fruit too low?
+                    //no
+                        //repeat no 2
+                    //yes
+                        //if any trials left
+                            //yes
+                                //repeat no 1
+                            //no
+                                //show game over, button text: Start Game
+
+
+//slice a fruit
+    //play sound
+    //explode fruit
